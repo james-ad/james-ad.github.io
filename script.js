fd6ef7a10ad2665424d7789a756a8693ec43066b
james-ad <muzikjay@gmail.com>
2018-05-22 08:09:52 -0700

    fixed css background cut-off problem

--- script.js
+++ script.js
@@ -23,6 +23,9 @@ window.addEventListener('load', function() {
         data = [];
         renderTableEntry();
     });
+    
+    renderTableEntry();
+    renderPercentage();
 
 //FUNCTION DECLARATIONS
     function depositCheck() {
@@ -89,10 +92,10 @@ window.addEventListener('load', function() {
             // Find value in Savings category of table, remove it from the data array and update the Savings Total section of the page.
             for (let entry of data) {
                 let rmValue = target.parentNode.previousSibling.previousSibling.innerText;
-                rmValue = rmValue.replace(/\,/g,'').replace(/\$/g, '');
+                rmValue = Number(rmValue.replace(/\,/g,'').replace(/\$/g, ''));
                 
-                if (rmValue == entry.savingsAmount) {
-                    let index = data.indexOf(rmValue);
+                if (rmValue === entry.savingsAmount) {
+                    let index = data.indexOf(entry);
                     data.splice(index, 1);
                     break;
                 }
@@ -100,6 +103,12 @@ window.addEventListener('load', function() {
         }
         renderTotal();
     };
+
+    // window.addEventListener('resize', function() {
+    //     let htmlPage = document.getElementById('htmlPage');
+    //     console.log(htmlPage);
+    //     htmlPage.style.height='100%';
+    // });
     
 });
 
